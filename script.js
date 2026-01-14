fa275db6c1e463c0701a72779c678469155ecc90
Saniya1595 <saniyapatel1509@gmail.com>
2026-01-14 19:48:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openLink(type) {
+  if (type === "linkedin") {
+    window.open("https://www.linkedin.com/in/saniya-patel-10a3a728a/", "_blank");
+  } 
+  else if (type === "github") {
+    window.open("https://github.com/Saniya1595", "_blank");
+  }
+  else if (type === "email") {
+    window.location.href = "mailto:saniyapatel1509@gmail.com";
+  }
+}
